98762ed295100dd45bea515cca262d3a3806112e
Chris Ramer <chrisramer.97@gmail.com>
2021-05-21 09:52:59 -0700

    Add create route & view for authors

--- Library/Controllers/AuthorsController.cs
+++ Library/Controllers/AuthorsController.cs
@@ -21,5 +21,18 @@ namespace Library.Controllers
 			List<Author> model = _db.Authors.ToList();
 			return View(model);
 		}
+
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Create(Author author)
+		{
+			_db.Authors.Add(author);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
 	}
 }
