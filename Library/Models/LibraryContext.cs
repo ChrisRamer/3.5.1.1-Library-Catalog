34fb1c03e7e34f10551ce64e729f1f389e0d8f33
Chris Ramer <chrisramer.97@gmail.com>
2021-05-21 09:35:53 -0700

    Add models & create database

--- Library/Models/LibraryContext.cs
+++ Library/Models/LibraryContext.cs
@@ -4,8 +4,9 @@ namespace Library.Models
 {
 	public class LibraryContext : DbContext
 	{
-		//public virtual DbSet<Library> XXX_Model1Name { get; set; }
-		//public virtual DbSet<Library> XXX_Model2Name { get; set; }
+		public virtual DbSet<Author> Authors { get; set; }
+		public virtual DbSet<Book> Books { get; set; }
+		public virtual DbSet<AuthorBook> AuthorBooks { get; set; }
 
 		public LibraryContext(DbContextOptions options) : base(options) { }
 
