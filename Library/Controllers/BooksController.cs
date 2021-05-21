b15070db2d1653319d18546a77f196d1efcf8946
Chris Ramer <chrisramer.97@gmail.com>
2021-05-21 11:16:24 -0700

    Add DeleteAuthor route for books

--- Library/Controllers/BooksController.cs
+++ Library/Controllers/BooksController.cs
@@ -110,5 +110,14 @@ namespace Library.Controllers
 			_db.SaveChanges();
 			return RedirectToAction("Details", new { id = book.BookId});
 		}
+
+		[HttpPost]
+		public ActionResult DeleteAuthor(int joinId)
+		{
+			AuthorBook joinEntry = _db.AuthorBooks.FirstOrDefault(entry => entry.AuthorBookId == joinId);
+			_db.AuthorBooks.Remove(joinEntry);
+			_db.SaveChanges();
+			return RedirectToAction("Details", new { id = joinEntry.AuthorId });
+		}
 	}
 }
